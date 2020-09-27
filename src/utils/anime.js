0522562581c09bae2d62e3543ba5a7f22a03de18
taha-rizwan <taharizwan492@gmail.com>
2020-09-28 03:44:43 +0500

    =Color changes and bugs fixed

--- src/utils/anime.js
+++ src/utils/anime.js
@@ -6,9 +6,15 @@ const Anime = (animeSearch, callback) => {
   request({url: url, json: true}, (error, {body}) =>{
     if(error) {
       callback(error, undefined)
-    }else if (body.request_cashed === false) {
-      throw new Error('Please provide a correct name!')
-    } else {
+    }else if (!body.results[1]) {
+      callback(undefined, {
+        title: 'Enter a correct name',
+        image_url: 'https://www.elegantthemes.com/blog/wp-content/uploads/2020/02/000-404.png',
+        score: 'none',
+        episodes: 'none'
+      }) 
+    }
+    else {
       callback(undefined, {
         title: (body.results[0].title),
          episodes:(body.results[0].episodes),
